c2dfb2a2d83fb52da7d230f73aaa1869af844864
PhilipLeeMinTar <philiplee98@gmail.com>
2023-04-12 00:28:09 +0800

    solved exercises 1-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) { //may take in any number of arguments
+    for (let i=1;i<arguments.length;i++){
+        let res = arr.indexOf(arguments[i]);
+        if (res != -1){
+            arr.splice(res, 1)
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    if (typeof first != "number" || typeof last != "number") return "ERROR";
+    if (first < 0 || last < 0) return "ERROR";
 
+    let sum = 0;
+    if (first < last){
+      for (let i=first;i<=last;i++){
+        sum += i;
+        }
+        return sum;  
+    } else {
+        for (let i=first;i>=last;i--){
+            sum += i;
+            }
+            return sum; 
+    }
+    
 };
 
 // Do not edit below this line
