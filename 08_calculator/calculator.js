20d754bf80de93a587de37b3a1f38c4fb2ae4e7f
PhilipLeeMinTar <philiplee98@gmail.com>
2023-05-19 00:01:17 +0800

    ex 8 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	for (const a of arr) {
+    sum += a;
+  }
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let mul = 1;
+  for (const a of arr) {
+    mul *= a;
+  }
 
+  return mul;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let mul = 1;
+  for (let i = 0; i<b; i++){
+    mul *= a;
+  }
+  return mul;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let res = 1;
+	for (let i=1;i<=n;i++){
+    res *= i;
+  }
+  return res;
 };
 
 // Do not edit below this line
