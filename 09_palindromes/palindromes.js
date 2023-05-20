189c79791ce45c8bae0c288aed0854c26844c423
PhilipLeeMinTar <philiplee98@gmail.com>
2023-05-20 13:24:20 +0800

    ex 9 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
 
+    let removedPunctuation = phrase.toLowerCase().replace(/\W|\s/g, "");
+    console.log(removedPunctuation);
+    let split = removedPunctuation.split("");
+    let reverse = split.reverse();
+    let join = reverse.join("");
+
+    console.log(join);
+    return removedPunctuation === join;
 };
 
 // Do not edit below this line
