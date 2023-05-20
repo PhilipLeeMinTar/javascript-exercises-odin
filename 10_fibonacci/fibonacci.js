30fa8a0c058373e1ffa175fe5e8104b369c4235a
PhilipLeeMinTar <philiplee98@gmail.com>
2023-05-20 16:15:38 +0800

    all exercises done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (typeof number === "string"){
+        number = parseInt(number);
+    }
+    if (number < 0) return "OOPS";
+    let seq = [1, 1] ;
+    let newNumber = 1;
+    for (let i=2; i<number;i++){
+        newNumber = seq[i-1] + seq[i-2];
+        seq.push(newNumber);
+    }
 
+    console.log(newNumber);
+    return newNumber;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(persons) {
+    const oldest = persons.reduce((max, person) => {
+        if (!person.yearOfDeath) person.yearOfDeath = (new Date()).getFullYear();
+        const age = person.yearOfDeath - person.yearOfBirth;
+        if (age > max) max = age;
+        return max;
+    }, 0)
+    console.log(oldest);
+    return persons.find(person => oldest == person.yearOfDeath - person.yearOfBirth);
+    
 };
 
 // Do not edit below this line
